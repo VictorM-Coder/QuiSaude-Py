846df709b26a786a2d8ab459f19c8e8a929ac00d
Victor Martins <84944695+VictorM-Coder@users.noreply.github.com>
2022-12-10 17:46:19 -0300

    views e consultas

---
+++ SQLs/create-tables-atualizado.sql
@@ -0,0 +1,102 @@
+
+CREATE TABLE endereco(
+    rua VARCHAR(100) NOT NULL,
+    bairro VARCHAR(50) NOT NULL,
+    numero VARCHAR(20) NOT NULL,
+    id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE horario_atendimento(
+    dia_semana CHAR(3) NOT NULL,
+	hora_abertura TIME NOT NULL,
+	hora_fechamento TIME NOT NULL,
+	id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE instituicao_saude(
+	cnpj VARCHAR(18) PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	id_endereco INT NOT NULL,
+	FOREIGN KEY(id_endereco) REFERENCES endereco(id)
+);
+
+CREATE TABLE instituicao_saude_telefone(
+	cnpj_instituicao_saude VARCHAR(18) NOT NULL,
+	telefone VARCHAR(20),
+	PRIMARY KEY(cnpj_instituicao_saude, telefone),
+	FOREIGN KEY(cnpj_instituicao_saude) REFERENCES instituicao_saude(cnpj)
+);
+
+CREATE TABLE instituicao_saude_horario_atendimento(
+    id SERIAL PRIMARY KEY,
+	id_horario INT NOT NULL,
+	cnpj_instituicao_saude VARCHAR(18),
+	FOREIGN KEY(cnpj_instituicao_saude) REFERENCES instituicao_saude(cnpj),
+	FOREIGN KEY(id_horario) REFERENCES horario_atendimento(id)
+);
+
+CREATE TABLE medico(
+	crm VARCHAR(9) PRIMARY KEY,
+	nome VARCHAR(150) NOT NULL,
+	especialidade VARCHAR(100) NOT NULL,
+	cnpj_instituicao VARCHAR(18) NOT NULL,
+	FOREIGN KEY(cnpj_instituicao) REFERENCES instituicao_saude(cnpj)
+);
+
+CREATE TABLE servico(
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(100) NOT NULL,
+	descricao TEXT,
+	preco REAL,
+	cnpj_instituicao_saude VARCHAR(18) NOT NULL,
+	FOREIGN KEY(cnpj_instituicao_saude) REFERENCES instituicao_saude(cnpj)
+);
+
+CREATE TABLE medico_presta_servico(
+    id SERIAL PRIMARY KEY,
+	crm_medico VARCHAR(9) NOT NULL,
+	id_servico INT NOT NULL,
+	FOREIGN KEY(crm_medico) REFERENCES medico(crm),
+	FOREIGN KEY(id_servico) REFERENCES servico(id)
+);
+
+CREATE TABLE medico_presta_servico_horario(
+    id SERIAL PRIMARY KEY,
+    id_medico_presta_servico INT NOT NULL,
+    id_horario_atendimento INT NOT NULL,
+    FOREIGN KEY(id_horario_atendimento) REFERENCES horario_atendimento(id),
+    FOREIGN KEY(id_medico_presta_servico) REFERENCES medico_presta_servico(id)
+);
+
+CREATE TABLE Campanha(
+    titulo VARCHAR(50),
+    descricao VARCHAR(255),
+    id SERIAL PRIMARY KEY,
+    id_endereco INT NOT NULL,
+    cnpj_instituicao VARCHAR(18) NOT NULL,
+    data_inicio DATE NOT NULL,
+    data_final DATE NOT NULL,
+    FOREIGN KEY(cnpj_instituicao) REFERENCES instituicao_saude(cnpj),
+    FOREIGN KEY(id_endereco) REFERENCES endereco(id)
+);
+
+CREATE TABLE campanha_horario_atendimento(
+    id SERIAL PRIMARY KEY,
+    id_campanha INT NOT NULL,
+    id_horario_atendimento INT NOT NULL,
+    FOREIGN KEY(id_campanha) REFERENCES Campanha(id),
+    FOREIGN KEY(id_horario_atendimento) REFERENCES horario_atendimento(id)
+);
+
+CREATE TABLE campanha_vacinacao(
+    dose VARCHAR(20),
+    id INT NOT NULL PRIMARY KEY,
+    FOREIGN KEY(id) REFERENCES Campanha(id)
+);
+
+CREATE TABLE faixaetaria(
+  idade_minima INT NOT NULL,
+  idade_maxima INT NOT NULL,
+  id_campanha_vanicao INT PRIMARY KEY,
+  FOREIGN KEY(id_campanha_vanicao) REFERENCES campanha_vacinacao(id)
+);
---
+++ SQLs/views.sql
@@ -0,0 +1,50 @@
+--VIEW 1
+CREATE VIEW nomes_instituices AS(
+    SELECT nome FROM instituicao_saude
+);
+
+--VIEW 2
+CREATE VIEW servicos_e_instituicoes AS(
+    SELECT instituicao_saude.nome as instituicao, servico.nome as nome_servico, servico.descricao, servico.preco
+    FROM instituicao_saude JOIN servico ON instituicao_saude.cnpj = servico.cnpj_instituicao_saude
+);
+
+--VIEW 3
+CREATE VIEW campanhas_vigentes AS (
+    SELECT * FROM campanha WHERE data_final > now()
+);
+
+--VIEW 4
+--está usando o mesmo do 2, porém com recursos de filtragem
+SELECT instituicao_saude.nome, servico.nome, servico.descricao, servico.preco
+FROM instituicao_saude JOIN servico ON instituicao_saude.cnpj = servico.cnpj_instituicao_saude
+;
+
+--VIEW 5
+CREATE VIEW medico_instituicao AS (
+    SELECT m.nome AS medico, m.crm, m.especialidade, i.nome AS instituicao FROM medico m INNER JOIN instituicao_saude i ON i.cnpj = m.cnpj_instituicao
+);
+
+--VIEW 6
+CREATE VIEW horario_instituicao AS (
+    SELECT instituicao_saude.nome, horario_atendimento.dia_semana, horario_atendimento.hora_abertura, horario_atendimento.hora_fechamento FROM
+           instituicao_saude INNER JOIN instituicao_saude_horario_atendimento ON instituicao_saude.cnpj = instituicao_saude_horario_atendimento.cnpj_instituicao_saude
+           INNER JOIN horario_atendimento ON instituicao_saude_horario_atendimento.id_horario = horario_atendimento.id
+);
+
+--VIEW 8
+CREATE VIEW endereco_instituicao AS (
+    SELECT instituicao_saude.nome, endereco.rua, bairro, numero
+    FROM instituicao_saude INNER JOIN endereco ON instituicao_saude.id_endereco = endereco.id
+);
+
+--VIEW 9
+CREATE VIEW contato_instituicao AS (
+    SELECT i.cnpj, i.nome AS instituicao, it.telefone FROM instituicao_saude_telefone it INNER JOIN instituicao_saude i ON i.cnpj = it.cnpj_instituicao_saude
+);
+
+--VIEW 10
+CREATE VIEW proximas_campanhas AS (
+    SELECT * FROM campanha WHERE data_inicio > now()
+);
+
